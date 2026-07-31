385b7ccb25621ae60d6916a56e314c93f23a4213
amal-j-antony <271721431+amal-j-antony@users.noreply.github.com>
2026-07-31 13:21:33 +0530

    31/07

--- frontend/src/User/Components/EditProfile.jsx
+++ frontend/src/User/Components/EditProfile.jsx
@@ -3,15 +3,15 @@ import { useEffect } from 'react'
 import { FaEdit } from 'react-icons/fa'
 import { toast } from 'react-toastify'
 import { updateUserProileAPI } from '../../services/allAPI.JS'
-import {useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../../services/axiosInstance'
 function EditProfile() {
     const navigate = useNavigate()
-    const [userId,setUserId] = useState("")
+    const [userId, setUserId] = useState("")
     const [offCanvas, setOffCanvas] = useState(false)
-    const [passwordMatch,setPasswordMatch] = useState(true)
-    const [preview,setPreview] = useState("")
-    const [existingUserImage,setExistingUserImage] = useState('')
+    const [passwordMatch, setPasswordMatch] = useState(true)
+    const [preview, setPreview] = useState("")
+    const [existingUserImage, setExistingUserImage] = useState('')
     const [userData, setUserData] = useState({
         username: "",
         // email: "",
@@ -22,20 +22,20 @@ function EditProfile() {
         email: ''
     })
     console.log(userData);
-    
+
 
     const inputEnter = (e, value) => {
         if (value == "cPassword") {
             setUserData({
                 ...userData,
-                [value]:e.target.value
+                [value]: e.target.value
             })
             userData.password == e.target.value ? setPasswordMatch(true) : setPasswordMatch(false)
 
-        } else if(value == "profileImage"){
+        } else if (value == "profileImage") {
             console.log(e.target.files[0]);
             const imageFile = e.target.files[0]
-            if(imageFile.type.startsWith("image/")){
+            if (imageFile.type.startsWith("image/")) {
                 setUserData({
                     ...userData,
                     profileImage: imageFile
@@ -43,7 +43,7 @@ function EditProfile() {
                 const imageURL = URL.createObjectURL(imageFile)
                 setPreview(imageURL)
             }
-        }else  {
+        } else {
             setUserData({
                 ...userData,
                 [value]: e.target.value
@@ -52,31 +52,44 @@ function EditProfile() {
     }
 
     const handleUpdate = async () => {
-        const { username, password ,cPassword ,bio ,profileImage } = userData
-        if(!username || !password || !cPassword || !bio){
-         toast.info("Please fill the form completely")
-        }else if(passwordMatch){
+        const { username, password, cPassword, bio, profileImage } = userData
+        if (!username || !password || !cPassword || !bio) {
+            toast.info("Please fill the form completely")
+        } else if (passwordMatch) {
             // toast.success("Ready for API")
             const reqBody = new FormData()
-            for(let key in userData){
-                if(key != profileImage){
+            for (let key in userData) {
+                if (key != profileImage) {
                     reqBody.append(key, userData[key])
-                }else{
-                    reqBody.append("profileImage",profileImage)
+                } else {
+                    reqBody.append("profileImage", profileImage)
                 }
             }
-            const result = await updateUserProileAPI(userId,reqBody)
+            const result = await updateUserProileAPI(userId, reqBody)
             console.log(result);
-            if(result.status == 200){
+            if (result.status == 200) {
                 toast.success("Proile updated successully")
-                sessionStorage.setItem("user",JSON.stringify(result.data))
-                setTimeout(()=>{
+                sessionStorage.setItem("user", JSON.stringify(result.data))
+                setTimeout(() => {
                     sessionStorage.clear()
                     navigate("/login")
-                },2500)
-                
+                }, 2500)
+
             }
         }
+    }
+
+    const handleReset = () => {
+        const data = JSON.parse(sessionStorage.getItem("user"))
+        setUserData({
+            password: "",
+            cPassword: '',
+            username: data?.username,
+            bio: data?.bio,
+            profileImage: data?.profileImage
+        })
+        setPreview('')
+        setPasswordMatch(true)
     }
 
     useEffect(() => {
@@ -91,11 +104,14 @@ function EditProfile() {
             })
             setUserId(data._id)
         }
-    },[])
+    }, [])
     return (
         <>
             <div>
-                <button onClick={() => setOffCanvas(true)} className='flex items-center gap-2 text-xl border p-2 rounded-xl hover:bg-black hover:text-white duration-500 cursor-pointer'>
+                <button onClick={() => {
+                    setOffCanvas(true)
+                    handleReset()
+                }} className='flex items-center gap-2 text-xl border p-2 rounded-xl hover:bg-black hover:text-white duration-500 cursor-pointer'>
                     Edit
                     <FaEdit />
                 </button>
@@ -110,12 +126,12 @@ function EditProfile() {
                         {/* body */}
                         <div className="flex justify-center items-center flex-col my-5">
                             <label htmlFor='uploadProfileImage' className='relative'>
-                                <input onChange={(e)=>inputEnter(e,"profileImage")} type="file" className='hidden' id='uploadProfileImage' />
+                                <input onChange={(e) => inputEnter(e, "profileImage")} type="file" className='hidden' id='uploadProfileImage' />
                                 {
                                     userData.profileImage ?
-                                    <img src={ preview ? preview : `${axiosInstance.defaults.baseURL}/uploads/${userData.profileImage}`} className='w-25 h-25 object-cover rounded-full' alt="" />
-                                    :
-                                    <img src={ preview ? preview : "https://res.cloudinary.com/dwaaoyztz/image/upload/v1783783482/user_s1wtzw.png"} className='w-25 h-25 object-cover rounded-full' alt="" />
+                                        <img src={preview ? preview : `${axiosInstance.defaults.baseURL}/uploads/${userData.profileImage}`} className='w-25 h-25 object-cover rounded-full' alt="" />
+                                        :
+                                        <img src={preview ? preview : "https://res.cloudinary.com/dwaaoyztz/image/upload/v1783783482/user_s1wtzw.png"} className='w-25 h-25 object-cover rounded-full' alt="" />
                                 }
                                 <div className='bg-black text-white px-3 py-2 rounded absolute right-0 top-20'><FaEdit className='' /></div>
                             </label>
@@ -126,11 +142,11 @@ function EditProfile() {
                         </div>
 
                         <div className="mt-2 mb-3 w-full px-5">
-                            <input onChange={(e) => inputEnter(e, "password")} type="password" placeholder='password' className='w-full border border-gray-300 rounded p-2' />
+                            <input onChange={(e) => inputEnter(e, "password")} value={userData.password} type="password" placeholder='password' className='w-full border border-gray-300 rounded p-2' />
                         </div>
 
                         <div className="mt-2 mb-3 w-full px-5">
-                            <input onChange={(e) => inputEnter(e, "cPassword")} type="password" placeholder='confirm password' className='w-full border border-gray-300 rounded p-2' />
+                            <input onChange={(e) => inputEnter(e, "cPassword")} value={userData.cPassword} type="password" placeholder='confirm password' className='w-full border border-gray-300 rounded p-2' />
                             {
                                 !passwordMatch && <p className='text-red-600'>Passwords do not match</p>
                             }
@@ -141,7 +157,7 @@ function EditProfile() {
                         </div>
 
                         <div className='flex justify-end w-full px-5 mt-5 gap-5'>
-                            <button className="bg-yellow-600 text-white py-2 px-3">Reset</button>
+                            <button onClick={handleReset} className="bg-yellow-600 text-white py-2 px-3">Reset</button>
                             <button onClick={handleUpdate} className="bg-green-600 text-white py-2 px-3">Update</button>
                         </div>
 
--- frontend/src/User/Components/UploadBooks.jsx
+++ frontend/src/User/Components/UploadBooks.jsx
@@ -1,43 +1,121 @@
 import React from 'react'
+import { useState } from 'react';
 import { BsCloudUploadFill } from "react-icons/bs";
 import { MdCloudUpload } from "react-icons/md";
+import { toast } from 'react-toastify';
+import { addBookAPI } from '../../services/allAPI.JS';
 
 function UploadBooks() {
+    const [bookDetails, setBookDetails] = useState({
+        bookTitle: "", publisher: "", author: "", isbn: "", imageURL: "", language: "", totalPages: "", category: "", price: "", discountPrice: "", abstract: "", uploadImages: []
+    })
+    const [preview, setPreview] = useState("")
+    const [previewList, setPreviewList] = useState([])
+    console.log(bookDetails);
+
+    const handleInput = (e, value) => {
+        console.log(value);
+
+        if (value == "uploadImages") {
+            if (previewList.length < 3) {
+                console.log(value);
+                const imageFile = e.target.files[0]
+                setBookDetails({
+                    ...bookDetails,
+                    [value]: [...bookDetails.uploadImages, imageFile]
+                })
+                const url = URL.createObjectURL(imageFile)
+                setPreview(url)
+                setPreviewList([...previewList, url])
+            }else{
+                toast.info("error")
+            }
+
+        } else {
+            setBookDetails({
+                ...bookDetails,
+                [value]: e.target.value
+            })
+        }
+    }
+
+    const handleUpload = async () => {
+        const { bookTitle, publisher, author, isbn, imageURL, language, totalPages, category, price, discountPrice, abstract ,uploadImages } = bookDetails
+        if(bookTitle&&
+            publisher&&
+            author&&
+            isbn&&
+            imageURL&&
+            language&&
+            totalPages&&
+            category&&
+            price&&
+            discountPrice&&
+            abstract&&
+            uploadImages.length>0
+        ){
+            const reqBody = new FormData()
+            for(let key in bookDetails){
+                if(key != "uploadImages"){
+                    reqBody.append(key,bookDetails[key])
+                }else{
+                    bookDetails.uploadImages.forEach(image => {
+                        reqBody.append("uploadImages",image)
+                    })
+                }
+            }
+            const result = await addBookAPI(reqBody)
+            if(result.status == 200){
+                toast.success('Book added successfully')
+            }
+        }else{
+            toast.info("Please fill all Fields")
+        }
+    }
+
+
     return (
         <>
             <div className="bg-gray-300 p-10">
                 <h1 className='text-center text-3xl'>Upload book details</h1>
                 <div className="grid grid-cols-2 gap-5 mt-10 w-full">
                     <div className="">
-                        <input type="text" placeholder='Book title' className='w-full bg-white p-2 my-5' />
-                        <input type="text" placeholder='Author' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Book Cover' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Total Pages' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Original Price' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Disocunt Price' className='w-full bg-white p-2 mb-5' />
-                        <textarea type="text" placeholder='Abstract' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.bookTitle} onChange={(e) => handleInput(e, "bookTitle")} type="text" placeholder='Book title' className='w-full bg-white p-2 my-5' />
+                        <input value={bookDetails.author} onChange={(e) => handleInput(e, "author")} type="text" placeholder='Author' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.imageURL} onChange={(e) => handleInput(e, "imageURL")} type="text" placeholder='Book Cover' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.totalPages} onChange={(e) => handleInput(e, "totalPages")} type="text" placeholder='Total Pages' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.price} onChange={(e) => handleInput(e, "price")} type="text" placeholder='Original Price' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.discountPrice} onChange={(e) => handleInput(e, "discountPrice")} type="text" placeholder='Disocunt Price' className='w-full bg-white p-2 mb-5' />
+                        <textarea value={bookDetails.abstract} onChange={(e) => handleInput(e, "abstract")} type="text" placeholder='Abstract' className='w-full bg-white p-2 mb-5' />
                     </div>
                     <div className="">
-                        <input type="text" placeholder='Book title' className='w-full bg-white p-2 my-5' />
-                        <input type="text" placeholder='Author' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Book Cover' className='w-full bg-white p-2 mb-5' />
-                        <input type="text" placeholder='Total Pages' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.publisher} onChange={(e) => handleInput(e, "publisher")} type="text" placeholder='Publisher' className='w-full bg-white p-2 my-5' />
+                        <input value={bookDetails.isbn} onChange={(e) => handleInput(e, "isbn")} type="text" placeholder='ISBN' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.language} onChange={(e) => handleInput(e, "language")} type="text" placeholder='Language' className='w-full bg-white p-2 mb-5' />
+                        <input value={bookDetails.category} onChange={(e) => handleInput(e, "category")} type="text" placeholder='Category' className='w-full bg-white p-2 mb-5' />
                         <label htmlFor="bookImages" className='w-full flex justify-center p-10'>
-                            <input type="file" hidden id='bookImages' />
-                            <MdCloudUpload className='text-[200px] cursor-pointer' />
+                            <input onChange={(e) => handleInput(e, "uploadImages")} type="file" hidden id='bookImages' />
+                            {preview ? <img src={preview} className='h-60' alt="" /> : <MdCloudUpload className='text-[200px] cursor-pointer' />}
                         </label>
                         <div className="flex justify-center items-center">
-                            <img src="https://picsum.photos/200" alt="" className='w-20 pe-5' />
-                            <img src="https://picsum.photos/200" alt="" className='w-20 pe-5' />
-                            <img src="https://picsum.photos/200" alt="" className='w-20 pe-5' />
-                            <label htmlFor="bookImages" className='flex justify-center p-3'>
-                                <input type="file" hidden id='bookImages' />
-                                <MdCloudUpload className='text-8xl cursor-pointer' />
-                            </label>
+                            {previewList &&
+                                <>
+                                    {
+                                        previewList.map((item) => (
+                                            <img key={item} src={item} alt="" className='w-20 pe-5' />
+                                        ))
+                                    }
+
+                                    {previewList.length < 3 &&
+                                        <label htmlFor="bookImagesSmallInput" className='flex justify-center p-3'>
+                                            <input onChange={(e) => handleInput(e, "uploadImages")} type="file" hidden id='bookImagesSmallInput' />
+                                            <MdCloudUpload className='text-8xl cursor-pointer' />
+                                        </label>}
+                                </>}
                         </div>
                         <div className="flex justify-center gap-5">
                             <button className='text-white bg-green-500 py-2 px-3'>Reset</button>
-                            <button className='text-white bg-blue-500 py-2 px-3'>Add Book Details</button>
+                            <button onClick={handleUpload} className='text-white bg-blue-500 py-2 px-3'>Add Book Details</button>
                         </div>
                     </div>
                 </div>
